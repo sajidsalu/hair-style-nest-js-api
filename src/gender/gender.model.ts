3cb6aff15d4047d0ed3450da833d13b7e200f0b6
sajid-M <sajid.m@experionglobal.com>
2024-01-16 11:57:40 +0530

    gender image urls added

--- src/gender/gender.model.ts
+++ src/gender/gender.model.ts
@@ -3,10 +3,12 @@ import * as mongoose from 'mongoose';
 export const genderSchema = new mongoose.Schema({
   name: { type: String, required: true },
   sortOrder: { type: Number, required: true },
+  imageUrl: { type: String, required: false },
 });
 
 export interface Gender extends mongoose.Document {
   id: string;
   name: string;
   sortOrder: number;
+  imageUrl: string;
 }
